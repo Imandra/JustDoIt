f2de0b249cfa227368bea9807375c5a75add0a76
Imandra <shalyapin.alexej@yandex.ru>
2016-12-06 21:03:53 +0400

    добавлены комментарии в handler.php

--- handler.php
+++ handler.php
@@ -1,19 +1,11 @@
 <?php
 $text = '';
 if (isset($_POST)) {
-    foreach ($_POST as $name => $value) {
-        $text .= $name . ": " . $value . "\r\n";
-    }
+    /*validation of the entered email address*/
     $to = $_POST['email'];
     if (!filter_var($to, FILTER_VALIDATE_EMAIL)) {
         die ('Введите корректный email!');
     }
-    /*stored form data in a text file*/
-    $file = fopen('app.txt', 'a+');
-    fwrite($file, 'date: ' . date('d-m-y, H-i-s') . "\r\n");
-    fwrite($file, $text . "" . "\r\n");
-    fclose($file);
-
     /*set php.ini - на хостинге НЕ ЗАБЫТЬ ЗАКОММЕНТИРОВАТЬ!!!*/
     ini_set("SMTP", "mail.interzet.ru");
     ini_set("smtp_port", "25");
@@ -28,8 +20,17 @@ if (isset($_POST)) {
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Date: " . date('D, d M Y h:i:s O') . "\r\n";
     $message = 'Это тестовое письмо';//на хостинге текст письма изменить на реальный
-
     mail($to, $subject, $message, $headers);
 
-    echo 'Данные формы сохранены, письмо на email: ' . $to . ' отправлено.';//ответ сервера
+    /*stored form data in a text file*/
+    foreach ($_POST as $name => $value) {
+        $text .= $name . ": " . $value . "\r\n";
+    }
+    $file = fopen('app.txt', 'a+');
+    fwrite($file, 'date: ' . date('d-m-y, H-i-s') . "\r\n");
+    fwrite($file, $text . "" . "\r\n");
+    fclose($file);
+
+    /*server response*/
+    echo 'Данные формы сохранены, письмо на email: ' . $to . ' отправлено.';
 }
